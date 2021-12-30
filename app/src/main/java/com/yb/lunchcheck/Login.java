1d923fc05b6acaa9c70870b7772dc1f19fc8ed3d
karenlee <klee070528@gmail.com>
2021-12-30 16:58:12 +0900

    a

--- app/src/main/java/com/yb/lunchcheck/Login.java
+++ app/src/main/java/com/yb/lunchcheck/Login.java
@@ -2,37 +2,79 @@ package com.yb.lunchcheck;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login);
 
-        TextView tv = findViewById(R.id.editTextTextPersonName6);
-        TextView textView = findViewById(R.id.editTextTextPersonName7);
-        TextView textView1 = findViewById(R.id.editTextTextPersonName8);
-        TextView textView2 = findViewById(R.id.editTextTextPersonName9);
-        Button btn = findViewById(R.id.button8);
+        TextInputEditText tv1 = findViewById(R.id.nameInput);
+        TextInputEditText tv2 = findViewById(R.id.usernameInput);
+        TextInputEditText tv3 = findViewById(R.id.schoolInput);
+        TextInputEditText tv4 = findViewById(R.id.passwordInput);
+        TextInputEditText tv5 = findViewById(R.id.emailInput);
+        Button btn = findViewById(R.id.submitLogin);
 
+        mAuth = FirebaseAuth.getInstance();
+        final String email;
+        String password;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText("click");
-                Intent it = new Intent(Login.this,Menu.class);
-                startActivity(it);
-                finish();
+                email = tv5.getText().toString();
+
             }
         });
 
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("sign up", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("sign up", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 
 }
